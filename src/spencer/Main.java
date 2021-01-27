be9f2e3197f1e611689be700da73419b47fb741b
spencerasj <spe19021@byui.edu>
2021-01-27 16:16:27 -0700

    said Hello to Connor for Prove 04

--- src/spencer/Main.java
+++ src/spencer/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Connor Mills.");
+    System.out.println("Hello Connor Mills, my name is Jennifer Spencer.");
     }
 }
